5f3b0cf59fd4edb3b89f2e4843ac33346d154d59
SainthCuico-UCM <saicuico@gmail.com>
2025-08-29 19:03:57 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Comments {
+   public static void main(String[] args) {
+      System.out.println("Java Comments."); 
+      
+      // Single-line comment
+      
+      /*
+       *  Multi-line comment
+       */
+
+      /**
+       *  Documentation/Javadoc comment
+       */    
+        
+   }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class FavoriteSong {
+   public static void main(String[] args) {
+      // Leave It All Behind (Acoustic) by Sleeping With Sirens
+      System.out.println("When you look at my life, tell me what do you see?");
+      System.out.println("I'm only human, so don't expect too much from me.");
+      System.out.println("I lost my faith, what have I become?");
+      System.out.println("I don't think I can be safe from what I'm running from.");
+   }
+}
