201cfaa4b34a76aff2644213c2d039e0009c60d8
SainthCuico-UCM <saicuico@gmail.com>
2025-08-31 21:37:54 +0800

    First Debugging Exercise

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,15 @@
+public class FixDebugOne {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+/*
+Bugs:
+1. Removed the space between "FixDebug" and "One" since class names in Java must be one single identifier.
+2. Wrong syntax for main method/entry point "Main" instead of "main".
+3. Missing brackets "[]" after String argument.
+4. Mispelled "Systm" instead of "System".
+5. Missing semicolon after println statement.
+6. Missing closing brace "}" to close code.
+*/
