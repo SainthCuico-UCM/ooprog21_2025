4817d473c2263cc5befb47af067054d1c4c354fd
SainthCuico-UCM <saicuico@gmail.com>
2025-09-19 08:54:14 +0800

    Activity No. 1: Using Methods (CompanyInfo)

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,15 @@
+class CompanyInfo
+{
+   public static void main(String[] a)
+   {
+      System.out.println("Smart Electronics");
+      displayHours();
+   }
+   
+   public static void displayHours()
+   {
+      System.out.println("Monday - Friday 8 AM to 6 PM");
+      System.out.println("Saturday        8 AM to Noon");
+      System.out.println("Sunday          Closed");
+   }
+}
